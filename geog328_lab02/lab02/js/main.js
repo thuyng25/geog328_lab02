5f29ef6c1524542ebdd9458e66a615fef7245dda
thuyng25 <100183732+thuyng25@users.noreply.github.com>
2025-10-23 22:07:06 -0700

    Add files via upload
    uploading folder workspace

---
+++ geog328_lab02/lab02/js/main.js
@@ -0,0 +1,46 @@
+// Get the element with the class "icon"
+let icon = document.getElementsByClassName("icon")[0];
+
+// Add an event listener for the 'click' event on the icon element
+icon.addEventListener('click', responsive_control);
+
+// Function to control the responsiveness of the navigation bar
+function responsive_control() {
+  // Get the element with the id "myTopnav"
+  let x = document.getElementById("myTopnav");
+
+  // Check if the class name of the element is "topnav"
+  if (x.className === "topnav") {
+    // If it is, add the "responsive" class to the element
+    x.className += " responsive";
+  } else {
+    // If it's not, remove the "responsive" class from the element
+    x.className = "topnav";
+  }
+  let slideIndex = 0;
+  showSlides();
+  
+  function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+  
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+  
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+  
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active-dot", "");
+    }
+  
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active-dot";
+  
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+  }
+
+}
+ 
